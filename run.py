79a57598ba64c7d67496ae26ec6a78a43e7410ab
aakrititalwar <aaku.12196@gmail.com>
2022-02-17 23:09:50 -0500

    first commit

---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from datetime import datetime
+import pytz
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def time():
+    timezone = pytz.timezone('America/New_York') 
+    timestamp = datetime.now(timezone)
+    return timestamp.strftime("%H:%M:%S")
+
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
